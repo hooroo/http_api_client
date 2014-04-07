0fa410fead5bf20154571e6e0b29bbff350b8998
Rob Monie <robmonie@gmail.com>
2014-04-07 14:15:20 +1000

    don't url encode log values

--- lib/http_api_client/timed_result.rb
+++ lib/http_api_client/timed_result.rb
@@ -11,7 +11,7 @@ class TimedResult
     log_entries = ["event_name=#{event}"]
     log_entries << "request_id=#{Thread.current[:request_id]}" if Thread.current[:request_id]
     log_entries << "timing=#{time}"
-    log_entries.concat(log_data.to_param.split('&'))
+    log_entries.concat(as_log_entries(log_data))
 
     HttpApiClient.logger.info(log_entries.join(", "))
 
@@ -19,6 +19,12 @@ class TimedResult
 
   def self.millis_since(start_time)
     (Time.now - start_time) * 1000
+  end
+
+  def self.as_log_entries(hash)
+    hash.inject [] do |result, entry|
+      result << "#{entry[0]}=#{entry[1]}"
+    end
   end
 
 end
